e3e374448c6efa41bfd82f833debb525934ba2bd
2Dsa3 <davsum04@hotmail.com>
2024-07-27 18:51:35 -0500

    guardado

--- 20Preguntas/src/main/java/ec/edu/espol/ventanas/VSubirArchivoController.java
+++ 20Preguntas/src/main/java/ec/edu/espol/ventanas/VSubirArchivoController.java
@@ -4,9 +4,14 @@
  */
 package ec.edu.espol.ventanas;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.FileChooser;
 
 /**
  * FXML Controller class
@@ -15,12 +20,34 @@ import javafx.fxml.Initializable;
  */
 public class VSubirArchivoController implements Initializable {
 
+    @FXML
+    private ImageView archivosRespuestas;
+    @FXML
+    private ImageView archivoPreguntas;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+        archivoPreguntas.setOnMouseClicked(e -> {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Seleccionar archivo de texto");
+        fileChooser.getExtensionFilters().add(
+            new FileChooser.ExtensionFilter("Archivos de texto", "*.txt")
+        );
+        //File archivoSeleccionado = fileChooser.showOpenDialog(primaryStage);
+        //if (archivoSeleccionado != null) {
+//            String contenido = leerArchivo(archivoSeleccionado);
+//            textArea.setText(contenido);
+
+            // Procesar el contenido del archivo para inicializar el juego
+//            HashMap<String, ArrayList<Integer>> animales = Reader.readerToHashMap(archivoSeleccionado.getPath());
+//            TreeG4 arbol = new TreeG4(animales);
+//            juego = new Game(arbol, 20);
+////            actualizarPregunta(lblPregunta);
+//        }
+    });  
+    }
 }
