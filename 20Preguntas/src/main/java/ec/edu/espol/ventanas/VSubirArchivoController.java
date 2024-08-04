f4b1ef69e2e078e0b5d048f64e6f1bebb922b452
Izaako04 <isaac.criollo04@gmail.com>
2024-08-03 19:29:20 -0500

    Subir archivos actualizado, botones funcionales

--- 20Preguntas/src/main/java/ec/edu/espol/ventanas/VSubirArchivoController.java
+++ 20Preguntas/src/main/java/ec/edu/espol/ventanas/VSubirArchivoController.java
@@ -5,17 +5,22 @@
 package ec.edu.espol.ventanas;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.scene.control.Label;
+import javafx.scene.text.Text;
 
 /**
  * FXML Controller class
@@ -25,7 +30,7 @@ import javafx.scene.control.Label;
 public class VSubirArchivoController implements Initializable {
 
     
-    private Label lblArchivoSeleccionado;
+    private Label lblArchivoSeleccionadoP;
     
     private File archivoSeleccionado;
     @FXML
@@ -36,6 +41,8 @@ public class VSubirArchivoController implements Initializable {
     private ImageView archivoPreguntas1;
     @FXML
     private ImageView archivoPreguntas2;
+    @FXML
+    private Text simboloduda;
 
 
     /**
@@ -43,26 +50,26 @@ public class VSubirArchivoController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
         
-
-    
+        configBtnArchivos();
+        
     }
     
-    private void handleCargarArchivo(MouseEvent event) {
+    private void handleCargarArchivo(MouseEvent event) throws IOException {
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Seleccionar archivo");
         fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Archivos de texto", "*.txt"));
         
-        // Obtener el Stage desde el evento del mouse
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        
         // Mostrar el cuadro de diálogo de selección de archivo
-        archivoSeleccionado = fileChooser.showOpenDialog(stage);
-        if (archivoSeleccionado != null) {
-            lblArchivoSeleccionado.setText("Archivo seleccionado: " + archivoSeleccionado.getName());
-        } else {
-            lblArchivoSeleccionado.setText("No se ha seleccionado ningún archivo");
+        File selectedFile = fileChooser.showOpenDialog(new Stage());
+        if (selectedFile != null) {
+            File targetFolder = new File("Txts");
+            if (!targetFolder.exists()) {
+                targetFolder.mkdirs(); // Crea la carpeta si no existe
+            }
+            File targetFile = new File(targetFolder, selectedFile.getName());
+            Files.copy(selectedFile.toPath(), targetFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+            
         }
     }
 
@@ -71,7 +78,50 @@ public class VSubirArchivoController implements Initializable {
         return archivoSeleccionado;
     }
 
-    private void subirArchivoPreguntas(MouseEvent event) {
+    private void subirArchivoPreguntas(MouseEvent event) throws IOException {
         handleCargarArchivo(event);
     }
+    
+    private void subirArchivoRespuestas(MouseEvent event) throws IOException {
+        handleCargarArchivo(event);
+    }
+    
+    private void configBtnArchivos(){
+        archivoPreguntas1.setOnMouseClicked(event -> {
+              try{
+                  subirArchivoPreguntas(event);
+                  muestraAlerta("Archivo", "Su archivo de Preguntas se ha subido con Exito!");
+                }catch (IOException ex) {
+                    ex.printStackTrace();
+                    }
+        });
+        
+        archivoPreguntas2.setOnMouseClicked((event -> {
+              try{
+                  subirArchivoRespuestas(event);
+                  muestraAlerta("Archivo", "Su archivo de Respuestas se ha subido con Exito!");
+                }catch (IOException ex) {
+                    ex.printStackTrace();
+                    }
+        }));
+        
+        simboloduda.setOnMouseClicked((event -> {
+              try{
+                  subirArchivoRespuestas(event);
+                  muestraAlerta("Archivo", "Su archivo de Respuestas se ha subido con Exito!");
+                }catch (IOException ex) {
+                    ex.printStackTrace();
+                    }
+        }));
+        
+    }
+    
+        private void muestraAlerta (String titulo, String mssg) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle(titulo);
+            alert.setHeaderText(null);
+            alert.setContentText(mssg);
+            alert.showAndWait();
+    }
+    
 }
